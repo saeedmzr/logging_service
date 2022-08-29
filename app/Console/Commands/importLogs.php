8c774177fd6ef4bda7cf4ac4249d71129daf701a
saeed <saeedmouzarmi@gmail.com>
2022-08-29 16:28:07 +0430

    add a trait for importing line by line

--- app/Console/Commands/importLogs.php
+++ app/Console/Commands/importLogs.php
@@ -3,12 +3,15 @@
 namespace App\Console\Commands;
 
 use App\Repositories\Log\LogRepository;
+use App\Traits\ImportLineTrait;
 use Carbon\Carbon;
 use Illuminate\Console\Command;
 use Symfony\Component\HttpFoundation\File\Exception\NoFileException;
 
 class importLogs extends Command
 {
+
+    use ImportLineTrait;
 
     private $logRepsoitory;
 
@@ -44,67 +47,24 @@ class importLogs extends Command
         $last_logged_at = $this->logRepsoitory->getLatestLogDateTime();
 
         try {
-            $lines = file(public_path('logs.txt'), true);
-
-            foreach ($lines as $line) {
-                $date = $this->getBetween($line, "[", "]");
-                $logged_at = Carbon::parse($date)->toDateTime();
-
-//                check if logged at is ok and we didnt have aleady saved it.
-                if (empty($last_logged_at) || Carbon::parse($last_logged_at)->timestamp < $logged_at->getTimestamp()) {
-//                   get service name
-                    $service = explode(' - -', $line)[0];
-//                  getstatus code
-                    $status_code = $this->getBetween($line, '" ', '\n');
-
-                    $method_endpont_version = $this->getBetween($line, '"', '"');
-
-//                  get method
-                    $method = explode(' ', $method_endpont_version)[0];
-
-//                   get endpoint
-                    $endpoint = explode(' ', $method_endpont_version)[1];
-
-//             get http version
-                    $http_version = explode(' ', $method_endpont_version)[2];
-
-                    $payload = [
-                        'service_name' => $service,
-                        'status_code' => $status_code,
-                        'logged_at' => $logged_at,
-                        'method' => $method,
-                        'endpoint' => $endpoint,
-                        'http_version' => $http_version,
-                    ];
-//              create our payload and store in DB
-                    $this->logRepsoitory->create($payload);
+            if ($file = fopen(public_path('logs.txt'), "r")) {
+                while (!feof($file)) {
+                    $line = fgets($file);
+                    if (!empty($line))
+                        $this->importLineToLog($line, $last_logged_at);
                 }
 
-
             }
+            fclose($file);
 
             $this->info("Logs Has been recorded successfully.");
+
         } catch (NoFileException $exception) {
             $this->info("The file doesn't exist");
         }
+
         return 0;
     }
 
-//    a customized fucntion for pulling our parametrs from that string
-
-    private function getBetween($string, $start = "", $end = ""): string
-    {
-        if (strpos($string, $start)) { // required if $start not exist in $string
-            $startCharCount = strpos($string, $start) + strlen($start);
-            $firstSubStr = substr($string, $startCharCount, strlen($string));
-            $endCharCount = strpos($firstSubStr, $end);
-            if ($endCharCount == 0) {
-                $endCharCount = strlen($firstSubStr);
-            }
-            return substr($firstSubStr, 0, $endCharCount);
-        } else {
-            return '';
-        }
-    }
 
 }
---
+++ app/Traits/ImportLineTrait.php
@@ -0,0 +1,75 @@
+<?php
+
+namespace App\Traits;
+
+use App\Models\Log;
+use Carbon\Carbon;
+
+trait ImportLineTrait
+{
+
+
+    public function importLineToLog(string $line, $last_logged_at)
+    {
+
+        $date = $this->getBetween($line, "[", "]");
+        $logged_at = Carbon::parse($date)->toDateTime();
+
+
+//                check if logged at is ok and we didnt have aleady saved it.
+        if (empty($last_logged_at) || Carbon::parse($last_logged_at)->timestamp < $logged_at->getTimestamp()) {
+
+//                   get service name
+            $service = explode(' - -', $line)[0];
+//                  getstatus code
+            $status_code = $this->getBetween($line, '" ', '\n');
+
+
+            $method_endpont_version = $this->getBetween($line, '"', '"');
+
+//                  get method
+            $method = explode(' ', $method_endpont_version)[0];
+
+//                   get endpoint
+            $endpoint = explode(' ', $method_endpont_version)[1];
+
+//             get http version
+            $http_version = explode(' ', $method_endpont_version)[2];
+
+            $payload = [
+                'service_name' => $service,
+                'status_code' => $status_code,
+                'logged_at' => $logged_at,
+                'method' => $method,
+                'endpoint' => $endpoint,
+                'http_version' => $http_version,
+            ];
+//              create our payload and store in DB
+            Log::create($payload);
+
+
+        }
+//        dd($line);
+
+
+    }
+
+
+//    a customized fucntion for pulling our parametrs from that string
+
+    private function getBetween($string, $start = "", $end = ""): string
+    {
+        if (strpos($string, $start)) { // required if $start not exist in $string
+            $startCharCount = strpos($string, $start) + strlen($start);
+            $firstSubStr = substr($string, $startCharCount, strlen($string));
+            $endCharCount = strpos($firstSubStr, $end);
+            if ($endCharCount == 0) {
+                $endCharCount = strlen($firstSubStr);
+            }
+            return substr($firstSubStr, 0, $endCharCount);
+        } else {
+            return '';
+        }
+    }
+
+}
